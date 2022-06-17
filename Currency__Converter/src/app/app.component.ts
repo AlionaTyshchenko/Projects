2792965fd95195f7a53b15b5f0d76213f211578c
AlionaTyshchenko <alionatyshchenko@gmail.com>
2022-06-17 22:27:30 +0300

    refactoring

--- Currency__Converter/src/app/app.component.ts
+++ Currency__Converter/src/app/app.component.ts
@@ -9,126 +9,130 @@ import { ApiRate, Country } from './interfaces';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  implements OnInit, OnDestroy{
+export class AppComponent implements OnInit, OnDestroy {
 
   rates!: ApiRate[]
   usd?: ApiRate
   euro?: ApiRate
-  aSub!:Subscription
+  aSub!: Subscription
   form!: FormGroup
   keysCountry!: Array<string>
-  rate?: ApiRate    
-  
+  rate?: ApiRate
+
   firstValue?: any
-  secondValue?:any
-  inputFirstValue:any
-  inputSecondValue:any
-  changedInputFirst:any
-  changedInputSecond:any
+  secondValue?: any
+  inputFirstValue: any
+  inputSecondValue: any
+  changedInputFirst: any
+  changedInputSecond: any
 
 
-  countries: Country[]=[
-    {name: 'UAH', currencyCode: 980},
-    {name:'USD', currencyCode: 840},
-    {name: 'EUR', currencyCode: 978}
+  countries: Country[] = [
+    { name: 'UAH', currencyCode: 980 },
+    { name: 'USD', currencyCode: 840 },
+    { name: 'EUR', currencyCode: 978 }
   ]
-  
+
   countryCodeFirst = this.countries[0].name
   countryCodeSecond = this.countries[1].name
 
   constructor(
     private exchangeService: ExchangeService
-    ){}
-    
-    ngOnInit() {
-      this.aSub = this.exchangeService.getCurrencyRate()
+  ) { }
+
+  ngOnInit() {
+    this.aSub = this.exchangeService.getCurrencyRate()
       .subscribe(res => {
         this.rates = res;
         this.usd = this.exchangeService.getRate(this.rates, this.getCurrencyCode('USD'), this.getCurrencyCode('UAH'))
         this.euro = this.exchangeService.getRate(this.rates, this.getCurrencyCode('EUR'), this.getCurrencyCode('UAH'))
         this.calcRate()
       })
-      
-      this.form = new FormGroup({
-        inputValue: new FormControl('', Validators.pattern('[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)')),
-      })
-      
-      this.keysCountry = this.countries.map((item:any) => item.name)
-    } 
+
+    this.form = new FormGroup({
+      inputValue: new FormControl('', Validators.pattern('^(0|[1-9]\d{0,2})(\.\d+)?$')),
+    })
+
+    this.keysCountry = this.countries.map((item: any) => item.name)
+  }
+
+  ngOnDestroy(): void {
+    this.aSub.unsubscribe()
+  }
+
+  getFirstSelectedCodeCountry(value: string) {
+    this.countryCodeFirst = value
+    this.calcRate()
+    this.exchangeCurrencyForPurchase()
+  }
+
+  getSecondSelectedCodeCountry(value: string) {
+    this.countryCodeSecond = value
+    this.calcRate()
+    this.exchangeCurrencyForPurchase()
+  }
+
+  calcRate() {
+    if (this.countryCodeFirst === this.countryCodeSecond) {
+      return
+    }
+    let codeA = this.getCurrencyCode(this.countryCodeFirst)
+    let codeB = this.getCurrencyCode(this.countryCodeSecond)
+
+    this.rate = this.exchangeService.getRate(this.rates, codeA, codeB)
+  }
+
+  getCurrencyCode(name: string) {
+    return this.countries.find(item => item.name === name)!.currencyCode;
+  }
+
+  exchangeCurrencyForPurchase() {
+    this.inputFirstValue = +this.firstValue
+
+    if (isNaN(this.inputFirstValue)) {
+      this.inputFirstValue = ''
+      return
+    }
+
+    if (this.countryCodeFirst === this.countryCodeSecond) {
+      this.changedInputFirst = this.inputFirstValue
+      return
+    }
     
-    ngOnDestroy(): void {
-      this.aSub.unsubscribe()
-    }
+    if (this.rate?.rateSell != null && this.rate?.rateBuy != null) {
 
-    getFirstSelectedCodeCountry(value:string) {
-      this.countryCodeFirst = value
-      this.calcRate()
-      this.exchangeCurrencyForPurchase()
-    }
-
-    getSecondSelectedCodeCountry(value:string) {
-      this.countryCodeSecond = value
-      this.calcRate()
-      this.exchangeCurrencyForPurchase()
-    }
-
-    calcRate(){
-      if(this.countryCodeFirst === this.countryCodeSecond){ 
-        return
-      } 
-      let codeA = this.getCurrencyCode(this.countryCodeFirst)
-      let codeB = this.getCurrencyCode(this.countryCodeSecond)
-      
-      this.rate = this.exchangeService.getRate(this.rates, codeA, codeB)
-    }
-  
-    getCurrencyCode(name:string) {
-      return  this.countries.find(item => item.name === name)!.currencyCode;
-    }
-
-    exchangeCurrencyForPurchase(){
-      this.inputFirstValue = +this.firstValue
-
-      if( !isNaN(this.inputFirstValue) ){
-
-        if(this.countryCodeFirst === this.countryCodeSecond){
-        this.changedInputFirst = this.inputFirstValue
-        return
-        }
-        if(this.rate?.rateSell != null && this.rate?.rateBuy != null) {
-
-          if(this.rate.currencyCodeA === this.getCurrencyCode(this.countryCodeFirst) ) {
-            this.changedInputFirst = (this.inputFirstValue  * this.rate?.rateBuy).toFixed(4)
-          } else {
-            this.changedInputFirst = (this.inputFirstValue  / this.rate?.rateSell).toFixed(4)
-          }
-        }
-      }else {
-        this.inputFirstValue = ''
+      if (this.rate.currencyCodeA === this.getCurrencyCode(this.countryCodeFirst)) {
+        this.changedInputFirst = (this.inputFirstValue * this.rate?.rateBuy).toFixed(4)
+      } else {
+        this.changedInputFirst = (this.inputFirstValue / this.rate?.rateSell).toFixed(4)
       }
     }
 
-    exchangeCurrencyForSale(){
-      this.inputSecondValue = +this.secondValue
+  }
 
-      if( !isNaN(this.inputSecondValue) ){
+  exchangeCurrencyForSale() {
+    this.inputSecondValue = +this.secondValue
 
-        if(this.countryCodeFirst === this.countryCodeSecond){
-          this.changedInputSecond = this.inputSecondValue
-          return
-        }
-        if(this.rate?.rateSell != null && this.rate?.rateBuy != null) {
+    if (isNaN(this.inputSecondValue)) {
+      this.inputSecondValue = ''
+      return
+    }
 
-          if(this.rate.currencyCodeA == this.getCurrencyCode(this.countryCodeFirst)){
-            this.changedInputSecond = (this.inputSecondValue / this.rate.rateBuy).toFixed(4)
-          } else {
-            this.changedInputSecond= (this.inputSecondValue * this.rate?.rateSell).toFixed(4)
-          }
-        } 
-      } else{
-        this.inputSecondValue = ''
+    if (this.countryCodeFirst === this.countryCodeSecond) {
+      this.changedInputSecond = this.inputSecondValue
+      return
+    }
+
+    if (this.rate?.rateSell != null && this.rate?.rateBuy != null) {
+
+      if (this.rate.currencyCodeA == this.getCurrencyCode(this.countryCodeFirst)) {
+        this.changedInputSecond = (this.inputSecondValue / this.rate.rateBuy).toFixed(4)
+      } else {
+        this.changedInputSecond = (this.inputSecondValue * this.rate?.rateSell).toFixed(4)
       }
     }
+  }
 }
+
 
 
